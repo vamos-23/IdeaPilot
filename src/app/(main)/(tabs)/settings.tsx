22c750d6b3ca082c3e0a4e67d997bc77b5f6d3d3
vamos-23 <172397434+vamos-23@users.noreply.github.com>
2025-11-30 23:15:05 +0530

    feat: create initial Settings screen layout

--- src/app/(main)/(tabs)/settings.tsx
+++ src/app/(main)/(tabs)/settings.tsx
@@ -1,7 +1,7 @@
 import { View, Text } from "react-native";
 export default function Dashboard() {
   return (
-    <View className="bg-green-500 flex-1">
+    <View className="bg-brandLight dark:bg-brandDark flex-1">
       <Text className="justify-center items-center">Settings</Text>
     </View>
   );

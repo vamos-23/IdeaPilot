04d966404c21c40567e50b9a2367dabc1e105471
vamos-23 <172397434+vamos-23@users.noreply.github.com>
2025-11-30 22:54:54 +0530

    feat: add bottom tab bar navigation and layout

--- src/app/(main)/(tabs)/_layout.tsx
+++ src/app/(main)/(tabs)/_layout.tsx
@@ -1,26 +1,120 @@
-//import { StyleSheet } from "react-native";
+import useThemeStore from "@/src/store/useThemeStore";
 import { Tabs } from "expo-router";
-//import { House, Sparkle, Settings } from "lucide-react-native";
-import { sc } from "../../../constants/responsive";
+import { LayoutList, Settings, Sparkles } from "lucide-react-native";
+import { StyleSheet, View } from "react-native";
+import { sc, vs } from "../../../constants/responsive";
 export default function TabsMainLayout() {
+  const { theme } = useThemeStore();
+  const isDark = theme === "dark";
   return (
     <Tabs
       screenOptions={{
         headerShown: false,
         tabBarStyle: {
-          backgroundColor: "green",
           position: "absolute",
-          elevation: 0,
+          elevation: 5,
           marginHorizontal: sc(30),
-          borderRadius: sc(35),
-          borderWidth: 2,
-          borderColor: "red",
+          marginBottom: vs(12),
+          backgroundColor: isDark ? "rgba(255,255,255,0.07)" : "#F5F5F5",
+          borderRadius: sc(30),
+          height: vs(55),
+          borderWidth: sc(1),
+          borderTopWidth: sc(1),
+          borderColor: isDark ? "rgba(255,255,255,0.23)" : "rgba(0,0,0,1)",
+          paddingBottom: vs(10),
+          paddingTop: vs(4.5),
+          overflow: "hidden",
+        },
+        tabBarActiveTintColor: isDark ? "#15cbeb" : "orangered",
+        tabBarInactiveTintColor: isDark
+          ? "rgba(255,255,255,0.60)"
+          : "#787878",
+        tabBarLabelStyle: {
+          fontFamily: "sans-serif",
+          fontWeight: "bold",
+          fontSize: sc(8.5),
         },
       }}
     >
-      <Tabs.Screen name="dashboard" />
-      <Tabs.Screen name="aisuggestion" />
-      <Tabs.Screen name="settings" />
+      <Tabs.Screen
+        name="dashboard"
+        options={{
+          title: "Dashboard",
+          tabBarIcon: ({ focused }) => (
+            <View style={styles.tabBarIcon}>
+              <LayoutList
+                stroke={
+                  focused
+                    ? isDark
+                      ? "#fff"
+                      : "mediumblue"
+                    : isDark
+                      ? "rgba(255,255,255,0.55)"
+                      : "rgba(0,0,0,0.55)"
+                }
+                size={22}
+                strokeWidth={focused ? sc(2) : sc(1.45)}
+              />
+            </View>
+          ),
+        }}
+      />
+
+      <Tabs.Screen
+        name="aisuggestion"
+        options={{
+          title: "AI Suggestions",
+          tabBarIcon: ({ focused }) => (
+            <View style={styles.tabBarIcon}>
+              <Sparkles
+                stroke={
+                  focused
+                    ? isDark
+                      ? "#fff"
+                      : "mediumblue"
+                    : isDark
+                      ? "rgba(255,255,255,0.55)"
+                      : "rgba(0,0,0,0.55)"
+                }
+                size={22}
+                strokeWidth={focused ? sc(2) : sc(1.45)}
+              />
+            </View>
+          ),
+        }}
+      />
+
+      <Tabs.Screen
+        name="settings"
+        options={{
+          title: "Settings",
+          tabBarIcon: ({ focused }) => (
+            <View style={styles.tabBarIcon}>
+              <Settings
+                stroke={
+                  focused
+                    ? isDark
+                      ? "#fff"
+                      : "mediumblue"
+                    : isDark
+                      ? "rgba(255,255,255,0.55)"
+                      : "rgba(0,0,0,0.55)"
+                }
+                size={22}
+                strokeWidth={focused ? sc(2) : sc(1.45)}
+              />
+            </View>
+          ),
+        }}
+      />
     </Tabs>
   );
 }
+
+const styles = StyleSheet.create({
+  tabBarIcon: {
+    margin: sc(3),
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});

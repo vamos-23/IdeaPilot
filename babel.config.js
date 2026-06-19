874b8884ac07ebc168fd8fb6ed34a8d0e0a99386
vamos-23 <172397434+vamos-23@users.noreply.github.com>
2026-06-19 12:00:45 +0530

    fix: dependency fixes applied

--- babel.config.js
+++ babel.config.js
@@ -7,7 +7,10 @@ module.exports = function (api) {
     ],
     plugins: [
       "react-native-worklets/plugin",
-      // "react-native-reanimated/plugin"
+      ["@babel/plugin-transform-class-properties", { loose: true }],
+      ["@babel/plugin-transform-private-methods", { loose: true }],
+      ["@babel/plugin-transform-private-property-in-object", { loose: true }],
+       //"react-native-reanimated/plugin"
     ],
   };
 };

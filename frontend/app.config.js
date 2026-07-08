4dccd4734e55f2d8609e959c86b52eef34bc7497
vamos-23 <172397434+vamos-23@users.noreply.github.com>
2026-07-08 15:17:24 +0530

    chore: track eas.json for cloud build configuration

--- frontend/app.config.js
+++ frontend/app.config.js
@@ -1,5 +1,7 @@
 import "dotenv/config";
 
+const isDev = process.env.APP_ENV === "development";
+
 export default ({ config }) => ({
   ...config,
   name: process.env.APP_NAME || "IdeaPilot",
@@ -10,26 +12,16 @@ export default ({ config }) => ({
   scheme: "ideapilot",
   userInterfaceStyle: "automatic",
   newArchEnabled: true,
-  /*
-  ios: {
-    supportsTablet: true,
-    splash: {
-      image: "./assets/images/logo.svg",
-      resizeMode: "contain",
-      backgroundColor: "#ffffff",
-      dark: {
-        image: "./assets/images/logo-dark.svg",
-        backgroundColor: "#000000"
-      }
-    },
-  },
-  */
+
   android: {
     package: process.env.ANDROID_PACKAGE || "com.vamos23.ideapilot",
-    googleServicesFile: process.env.GOOGLE_SERVICES_JSON,
+
+    googleServicesFile: isDev
+      ? process.env.GOOGLE_SERVICES_JSON_DEV
+      : process.env.GOOGLE_SERVICES_JSON_PROD,
+
     softwareKeyboardLayoutMode: "resize",
     launchMode: "singleTask",
-
     adaptiveIcon: {
       foregroundImage: "./assets/images/android-icon-foreground.png",
       backgroundImage: "./assets/images/android-icon-background.png",
@@ -77,12 +69,26 @@ export default ({ config }) => ({
     eas: {
       projectId: "37955152-0da1-41b9-a735-dfc819e7f784",
     },
-    FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
-    FIREBASE_AUTH_DOMAIN: process.env.FIREBASE_AUTH_DOMAIN,
-    FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
-    FIREBASE_STORAGE_BUCKET: process.env.FIREBASE_STORAGE_BUCKET,
-    FIREBASE_MESSAGING_SENDER_ID: process.env.FIREBASE_MESSAGING_SENDER_ID,
-    FIREBASE_APP_ID: process.env.FIREBASE_APP_ID,
-    FIREBASE_MEASUREMENT_ID: process.env.FIREBASE_MEASUREMENT_ID,
+    FIREBASE_API_KEY: isDev
+      ? process.env.DEV_FIREBASE_API_KEY
+      : process.env.FIREBASE_API_KEY,
+    FIREBASE_AUTH_DOMAIN: isDev
+      ? process.env.DEV_FIREBASE_AUTH_DOMAIN
+      : process.env.FIREBASE_AUTH_DOMAIN,
+    FIREBASE_PROJECT_ID: isDev
+      ? process.env.DEV_FIREBASE_PROJECT_ID
+      : process.env.FIREBASE_PROJECT_ID,
+    FIREBASE_STORAGE_BUCKET: isDev
+      ? process.env.DEV_FIREBASE_STORAGE_BUCKET
+      : process.env.FIREBASE_STORAGE_BUCKET,
+    FIREBASE_MESSAGING_SENDER_ID: isDev
+      ? process.env.DEV_FIREBASE_MESSAGING_SENDER_ID
+      : process.env.FIREBASE_MESSAGING_SENDER_ID,
+    FIREBASE_APP_ID: isDev
+      ? process.env.DEV_FIREBASE_APP_ID
+      : process.env.FIREBASE_APP_ID,
+    FIREBASE_MEASUREMENT_ID: isDev
+      ? process.env.DEV_FIREBASE_MEASUREMENT_ID
+      : process.env.FIREBASE_MEASUREMENT_ID,
   },
 });
